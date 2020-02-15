d5c1558411dd24d73c2dba30ba31d7381a347059
Havi <1058695900@qq.com>
2020-02-15 15:59:21 +0800

    去掉Pods

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:vendor_manager/vendor_manager.dart';
 
 void main() => runApp(MyApp());
 
